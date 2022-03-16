ae74be13c234a07fa3c7a71ccb606148e4cd4498
fahad <fadiformanite@gmail.com>
2022-03-16 22:59:21 +0500

    app invoke issue resolved and changed folders to appB

---
+++ FCPartner/src/test/java/AndroidAppB/AppiumFramework/SmokeTestScenarios.java
@@ -0,0 +1,41 @@
+package AndroidAppB.AppiumFramework;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import AndroidAppB.AppiumFramework.Base;
+import PageObjects.LoginScreen;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class SmokeTestScenarios extends Base{
+	public static AndroidDriver<AndroidElement> driver;
+	public static LoginScreen login;
+
+	@BeforeClass
+	public void setUp() throws IOException, InterruptedException
+	{
+		Runtime.getRuntime().exec("taskkill /F /IM node.exe");
+		Thread.sleep(3000);
+		service = startServer();
+		driver = capabilities("appB");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		login = new LoginScreen(driver);
+		
+	}
+
+	@Test (priority=1)
+	public void login()
+		{
+			//To Write appB Test Scenarios
+		}
+	
+			
+	@AfterClass
+	public void tearDown()
+	{
+		service.stop();
+	}
+}
